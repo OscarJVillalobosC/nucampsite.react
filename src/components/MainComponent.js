4fe76bd39e6a06e5f4a0a4e9a205d64bd17a52f7
Oscar Villalobos <oscarjvillalobosc@gmail.com>
2021-05-20 18:21:11 -0400

    Bread Crumbs and React Router parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Directory from './DirectoryComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
@@ -34,6 +35,15 @@ class Main extends Component {
             );
         }
 
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        };    
+
         return (
             <div>
                 <Header />
@@ -41,8 +51,9 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory 
                      campsites={this.state.campsites} />} />
-                    <Redirect to='/home' />
+                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Redirect to='/home' />
                 </Switch>
                 <Footer />
             </div>
